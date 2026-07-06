114a299d7bdef1e79cefc12946f2908662b2efe6
f1yweight <flyweight.hota@gmail.com>
2026-07-07 00:35:29 +0200

    Implement User module with Flyway integration

---
+++ src/main/java/com/flyweight/poker_dashboard/user/User.java
@@ -0,0 +1,24 @@
+package com.flyweight.poker_dashboard.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/flyweight/poker_dashboard/user/UserController.java
@@ -0,0 +1,23 @@
+package com.flyweight.poker_dashboard.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+}
