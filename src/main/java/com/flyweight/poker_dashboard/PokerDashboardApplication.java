6d2604882f05074eec23625b224adfaf960a0487
f1yweight <flyweight.hota@gmail.com>
2026-07-06 22:45:50 +0200

    Initial project setup

---
+++ src/main/java/com/flyweight/poker_dashboard/PokerDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.flyweight.poker_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flyweight/poker_dashboard/PokerDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyweight.poker_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
